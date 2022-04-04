6f7ad038fac54aed9b8715743c4270473e816b4b
yoonjunkyo <yoonjunkyo@gmail.com>
2022-04-04 22:31:32 +0900

    Add question files

--- 2회/2_5.cpp
+++ 2회/2_5.cpp
@@ -5,33 +5,31 @@ using namespace std;
 int N = 8;
 int D = 4;
 int K = 3;
-int ch[4];
+vector<int>student = { 1,6,4,3,0,3,14,12 };
+vector<int>g_pow = { 1,2,4,8 };
+int g_bit = 0;
+int cnt = 0;
 
-void DFS(int L, int S,int K)
-{
-    if (L == K) 
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            cout << ch[i] << " ";
-        }
-        cout << endl;
-    }
-    else
-    {
-        for (int i = S; i < D; i++) 
-        {
-            ch[i] = 1;
-            DFS(L + 1, i + 1, K);
-            ch[i] = 0;
-        }
-    }
-
+void DFS(int L,int S) {
+	if (L == K) {
+		for (int i = 0; i < student.size(); i++) {
+			if ((g_bit & student[i]) == student[i]) cnt++;
+			cout << (g_bit & student[i])<<" ";
+		}
+		cout <<"g_bit : "<< g_bit <<" cnt : "<< cnt <<endl;
+		cnt = 0;
+	}
+	else {
+		for (int i = S; i < D; i++) {
+			g_bit = g_bit + g_pow[i];
+			DFS(L + 1, i + 1);
+			g_bit = g_bit - g_pow[i];
+		}
+	}
 }
 int main()
 {
+	DFS(0, 0);
 
-    DFS(0, 0, K);
-
-    return 0;
+	return 0;
 }
--- 6회/6_3.cpp
+++ 6회/6_3.cpp
@@ -1,41 +1,36 @@
 ﻿#include <iostream>
 #include <vector>
+
 using namespace std;
+
+int n = 6;
+int m = 8;
+vector<string>board = {
+    "00001010",
+    "00011110",
+    "00011000",
+    "00011110",
+    "11111110",
+    "11111110"
+};
+vector<vector<int>> dy(n, vector<int>(m, 0));
 
 int main()
 {
-	int arr[10][10] = { 
-	{0,0,0,0,1,0,1,0},
-	{0,0,0,1,1,1,1,0},
-	{0,0,0,1,1,0,0,0},
-	{0,0,0,1,1,1,1,0},
-	{1,1,1,1,1,1,1,0},
-	{1,1,1,1,1,1,1,0}
-	};
-	vector<vector<int>> dy(7, vector<int>(9, 0));
-	for (int i = 1; i < 7; i++) {
-		for (int j = 1; j < 9; j++) {
-			if (arr[i-1][j-1] == 1) {
-				dy[i][j] = dy[i - 1][j] + dy[i][j - 1] - dy[i - 1][j - 1] +1;
-				int x = j;
-				int y = i;
-				for (int k = 1; k < min(x, y); k++) {
-					if (dy[i - k][j - k] == 1) {
-						dy[i][j] = dy[i][j] + 1;
-					}
-				}
-			}
-			else
-			{
-				dy[i][j] = dy[i - 1][j]+dy[i][j - 1]- dy[i-1][j - 1];
-			}
-
-		}
-	}
-	for (int i = 1; i < 7; i++) {
-		for (int j = 1; j <9; j++) {
-			cout << dy[i][j]<<" ";
-		}cout << endl;
-	}
-	return 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == '0') {
+                cout << 0 << " ";
+                continue;
+            }
+            int a = 0, b = 0, c = 0;
+            if (i >= 1) a = dy[i - 1][j];
+            if (j >= 1) b = dy[i][j - 1]; 
+            if (j >= 1 && i >= 1) c = dy[i-1][j - 1];
+            dy[i][j] = min(a, min(b, c))+1;
+            cout << dy[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
